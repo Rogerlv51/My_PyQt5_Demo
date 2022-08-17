1135b783a9b70f9fc627fc27812d0b3f108296c4
Rogerlv51 <949608370@qq.com>
2022-08-17 17:35:51 +0800

    first_demo

--- 01_firstdemo.py
+++ 01_firstdemo.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt5.Qt import *
+# 注意这里导包的时候不是原仓库作者写的PyQt5.Qt，这个已经找不到了，删除了，采用下面的import方式
+from PyQt5.QtWidgets import *
 
 # 几乎所有qt程序都要先创建一个应用
 app = QApplication(sys.argv)
@@ -10,13 +11,16 @@ window = QWidget()
 # 设置窗口名称
 window.setWindowTitle("第一个PyQt程序")
 # 窗口大小
-window.resize(500, 500)
+window.resize(600, 600)
 # 窗口弹出的初始位置（即我们电脑界面上看到的位置）
 window.move(800, 250)
 
-label = QLabel(window)
+label = QLabel(window)     # 一个文本标签，需要绑定窗口才能显示
 label.setText("Hello world")
-label.move(200, 240)
+label.move(250, 250)       # 相对于window的位置
+button = QPushButton(window)
+button.setText("按压")
+button.move(220, 300)
 
 window.show()
 
